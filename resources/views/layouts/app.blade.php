164869084cfd521e9cc471470e14c947beac866e
Jherald Vibar <vibar_jherald@spcc.edu.ph>
2025-04-21 21:40:52 +0800

    Complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <title>{{config('app.name')}}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="icon" href="{{asset('img/spcc.png')}}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
     rel="stylesheet">
